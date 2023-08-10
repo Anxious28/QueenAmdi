d827f1d0a232d10457c4f066ff1c20eddcadce77
Anxious28 <107701954+Anxious28@users.noreply.github.com>
2023-08-10 07:27:44 -0700

    amdiModule.js

--- assets/amdiModule.js
+++ assets/amdiModule.js
@@ -1,23 +1,19 @@
-/**
-░██████╗░██╗░░░██╗███████╗███████╗███╗░░██╗  ░█████╗░███╗░░░███╗██████╗░██╗
-██╔═══██╗██║░░░██║██╔════╝██╔════╝████╗░██║  ██╔══██╗████╗░████║██╔══██╗██║
-██║██╗██║██║░░░██║█████╗░░█████╗░░██╔██╗██║  ███████║██╔████╔██║██║░░██║██║
-╚██████╔╝██║░░░██║██╔══╝░░██╔══╝░░██║╚████║  ██╔══██║██║╚██╔╝██║██║░░██║██║
-░╚═██╔═╝░╚██████╔╝███████╗███████╗██║░╚███║  ██║░░██║██║░╚═╝░██║██████╔╝██║
-░░░╚═╝░░░░╚═════╝░╚══════╝╚══════╝╚═╝░░╚══╝  ╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═════╝░╚═╝
+                                           
+ ▀█▀ █░█ █▀▀ █▀█ █░█ ▄▀█ █▄░█ ▀█▀ █▀█ █▀▄▀█
+ ░█░ █▀█ ██▄ █▀▀ █▀█ █▀█ █░▀█ ░█░ █▄█ █░▀░█
  __  __       _ _   _       ____             _          
 |  \/  |_   _| | |_(_)     |  _ \  _____   _(_) ___ ___ 
 | |\/| | | | | | __| |_____| | | |/ _ \ \ / / |/ __/ _ \
 | |  | | |_| | | |_| |_____| |_| |  __/\ V /| | (_|  __/
 |_|  |_|\__,_|_|\__|_|     |____/ \___| \_/ |_|\___\___|
-* @project_name Queen Amdi [WA Multi-device]
-* @author BlackAmda <https://github.com/BlackAmda>
+* @project_name The Phantom [WA Multi-device]
+* @author Annix <https://github.com/Anxious28>
 * @description A WhatsApp based 3ʳᵈ party application that provide many services with a real-time automated conversational experience
 * @link <https://github.com/BlackAmda/QueenAmdi>
 * @version 4.0.8
 * @file  amdiModule.js - QueenAmdi bot module and Web WA connection
 
-© 2022 Black Amda, ANTECH. All rights reserved.
+© 2023 Phantoms, ANTECH. All rights reserved.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.*/
 
